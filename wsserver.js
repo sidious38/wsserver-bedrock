0d655b1db73a3ce4352fd8347689a64ad725f5bf
sidious38 <sidious38@protonmail.com>
2021-10-25 14:39:00 +0200

    wsserver v2.1 update Fixed the bug where the message from the client removed the question/input field

--- wsserver.js
+++ wsserver.js
@@ -4,70 +4,69 @@ const WebSocket = require('ws');
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
-const wss = new WebSocket.Server({ port: 8000 });
+const wss = new WebSocket.Server({port: 8000});
+
 try {
-  console.log("Server is running on your_ip:8000");
+    console.log("Server is running on your_ip:8000");
 
-  wss.on('connection', socket => {
-    console.log("A client is opening a connection");
+    wss.on('connection', socket => {
+        console.log("A client is opening a connection");
 
-    const command = () => {
-      return new Promise((resolve) => {
-        rl.question(`Command: /`, (cmd) => {
+        const command = () => {
+            return new Promise((resolve) => {
+                rl.question(`Command: /`, (cmd) => {
 
-          // log user details
-          //console.log(`\nYour INPUT: /${cmd}`);
+                    // log user details
+                    //console.log(`\nYour INPUT: /${cmd}`);
 
-          // Send command
-          socket.send(JSON.stringify(
-            {
-              "body": {
-                "origin": {
-                  "type": "player"
-                },
-                "commandLine": `${cmd}`,
-                "version": 1
-              },
-              "header": {
-                "requestId": "00000000-0000-0000-0000-000000000000",
-                "messagePurpose": "commandRequest",
-                "version": 1,
-                "messageType": "commandRequest"
-              }
-            }
-          ));
+                    // Send command
+                    socket.send(JSON.stringify({
+                            "body": {
+                                "origin": {
+                                    "type": "player"
+                                },
+                                "commandLine": `${cmd}`,
+                                "version": 1
+                            },
+                            "header": {
+                                "requestId": "00000000-0000-0000-0000-000000000000",
+                                "messagePurpose": "commandRequest",
+                                "version": 1,
+                                "messageType": "commandRequest"
+                            }
+                        }
+                    ));
 
-          console.log("Command sent\n\n");
+                    console.log("Command sent\n\n");
 
-          resolve();
+                    resolve();
+                });
+            });
+        }
+
+        async function askCommand() {
+            await command();
+        }
+
+        // Run async function once
+        askCommand();
+
+        socket.on('message', async function incoming(message) {
+            console.log('\nreceived: %s', message);
+            await askCommand();
         });
-      });
-    }
 
-    async function askCommands() {
-      while (true) {
-        await command();
-      }
-    }
-    
-    // Run async function
-    askCommands();
-
-    socket.on('message', function incoming(message) {
-      console.log('\nreceived: %s', message);
+        process.on('SIGINT', function () {
+            console.log("\nInterrupt Ctrl-C");
+            process.exit();
+        });
     });
 
-    process.on('SIGINT', function() {
-      console.log("\nInterrupt Ctrl-C");
-      process.exit();
-    });
-  });
-
-  wss.off;
-} catch(E) {
-  console.log(E);
+    wss.off;
+} catch (E) {
+    console.log(E);
 }
